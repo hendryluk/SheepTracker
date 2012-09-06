68374fbb50966b8d63da601222348f2034fa84f4
Hendry Luk <hendrymail@gmail.com>
2012-09-07 01:06:02 +1000

    Converted index.cshtml page into single-page dojo

---
+++ SheepTracker.Web/Scripts/app/WorkItemListWidget.js
@@ -0,0 +1,6 @@
+﻿define(['dojo/_base/declare', 'dijit/_WidgetBase', 'dijit/_TemplatedMixin', 'dojo/text!./templates/WorkItemListWidget.html'],
+    function(declare, _WidgetBase, _TemplatedMixin, template) {
+        return declare([_WidgetBase, _TemplatedMixin], {
+            templateString: template
+        });
+});
