ec76fee9a128c09b12777205890da7129e1953f0
Hendry Luk <hendrymail@gmail.com>
2012-09-10 13:56:21 +1000

    Got dojo mvc binding works with the newer style

--- SheepTracker.Web/Scripts/app/WorkItemListWidget.js
+++ SheepTracker.Web/Scripts/app/WorkItemListWidget.js
@@ -3,34 +3,19 @@
         'dojox/mvc/Group', 'dijit/form/TextBox', 'dojox/mvc/Output', 'dojox/mvc/Repeat', 'dojox/mvc/at'],
     function (declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template,
         Observable, mvc, Rest, rx) {
+        
         return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
             templateString: template,
             constructor: function () {
-                this.model = mvc.newStatefulModel({
-                    data: {searchResults: [{a: 'ccc'}], searchTerms: "123"}
-                });
+                this.model = mvc.getStateful({ searchResults: [], searchTerms: "123" });
             },
             postCreate: function () {
-                var searchRest = new Rest("api/WorkItems", true);
-                //this.txSearch.watch('value', dojo.hitch(this, function () { alert(this.model.searchTerms); }));
-
-                //this.model.searchTerms.watch(dojo.hitch(this, function () {
-                //    debugger; alert(this.model.searchTerms); }));
-                //this.model.watch("searchTerms", function () { alert('aaa'); });
-                //this.model.searchTerms.set('value', 'aaa');
-
-                //this.model.searchTerms.set('type', 'submit');
-
-                //this.model.searchTerms.set([mvc.newStatefulModel({ a: "vvv" })]);
-                //this.model.searchResults.add(1, mvc.newStatefulModel({ data: { a: "vvv" } }));
-                //this.model.searchResults.set([mvc.newStatefulModel({ data: { a: "vvv" } })]);
-                this.model.searchResults.set([mvc.newStatefulModel({ data: { a: "vvv" } })]);
-
-
-                rx.watch(this.model.searchTerms, 'value')
-                    .throttle(200)
+                var searchRest = new Rest("/SheepTracker/api/WorkItems", true);
+                
+                rx.watch(this.model, 'searchTerms')
+                    .throttle(300)
                     .select(dojo.hitch(this, function () {
-                        return this.txSearch.value;
+                        return this.model.searchTerms;
                     }))
                     .distinctUntilChanged()
                     .where(function (q) { return q.length > 0; })
@@ -39,14 +24,7 @@
                     })
                     .switchLatest()
                     .subscribe(dojo.hitch(this, function (data) {
-                        var result = this.model.searchResults;
-                        debugger;
-                        this.model.searchResults.set([mvc.newStatefulModel({ data: { a: "bbb" } })]);
-                        debugger;
-
-                        //this.model.searchResults.set("data", mvc.newStatefulModel({ data: data }));
-                        //this.model.searchResults.add(1, mvc.newStatefulModel({data: data[0]}));
-                        //this.model.set("searchResults", mvc.newStatefulModel({data: data}));
+                        this.model.set("searchResults", mvc.getStateful(data));
                     }));
             }
         });
