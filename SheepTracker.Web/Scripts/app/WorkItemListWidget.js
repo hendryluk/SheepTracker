d944ae124bc583ab4a3d7b3895e26f935a198012
Hendry Luk <hendrymail@gmail.com>
2012-09-21 15:28:30 +1000

    Refactor javascript

--- SheepTracker.Web/Scripts/app/WorkItemListWidget.js
+++ SheepTracker.Web/Scripts/app/WorkItemListWidget.js
@@ -12,35 +12,10 @@
     function (declare, domStyle,
         _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template,
         mvc, Rest, rx) {
-
-        var keyboardNavigationMixin = declare(null, {
-            _isNavigatingList: null,
-            startup: function() {
-                this.inherited(arguments);
-                rx.on(window, "keydown")
-                    .subscribe(dojo.hitch(this, function(evt) {
-                        switch (evt.keyCode) {
-                        case dojo.keys.DOWN_ARROW:
-                            evt.preventDefault();
-                            this._selectDown();
-                            break;
-                        case dojo.keys.UP_ARROW:
-                            evt.preventDefault();
-                            this._selectUp();
-                            break;
-                        case dojo.keys.ENTER:
-                            evt.preventDefault();
-                            this._focusNavigationToSelection();
-                            break;
-                        }
-                    }));
-            },
-            _focusNavigationToSelection: function() {
-                var selectedWidget = dijit.byNode(dojo.query(".selectable", this.domNode)[0]);
-                (selectedWidget.onPressedEnter||function(){})();
-            },
-            _selectDown: function() {
-                var selectables = dojo.query(".selectable", this.domNode);
+        
+        function initKeyboardNavigation(self) {
+            function selectDown() {
+                var selectables = dojo.query(".selectable", self.domNode);
                 var selected = selectables.filter(".selected")[0];
                 var selectedIndex = selectables.indexOf(selected);
 
@@ -48,9 +23,10 @@
                     dojo.removeClass(selected, "selected");
                     dojo.addClass(selectables[selectedIndex + 1], "selected");
                 }
-            },
-            _selectUp: function() {
-                var selectables = dojo.query(".selectable", this.domNode);
+            }
+
+            function selectUp() {
+                var selectables = dojo.query(".selectable", self.domNode);
                 var selected = selectables.filter(".selected")[0];
                 var selectedIndex = selectables.indexOf(selected);
 
@@ -59,66 +35,96 @@
                     dojo.addClass(selectables[selectedIndex - 1], "selected");
                 }
             }
-        });
+            
+            function selectRight(evt) {
+                if (self.txSearch.textbox.selectionEnd < self.txSearch.value.length)
+                    return;
+
+                evt.preventDefault();
+                var selectedWidget = dijit.byNode(dojo.query(".selectable", self.domNode)[0]);
+            }
+
+            rx.on(window, "keydown")
+                .subscribe(function (evt) {
+                    switch (evt.keyCode) {
+                        case dojo.keys.DOWN_ARROW:
+                            evt.preventDefault();
+                            selectDown();
+                            break;
+                        case dojo.keys.UP_ARROW:
+                            evt.preventDefault();
+                            selectUp();
+                            break;
+                        case dojo.keys.RIGHT_ARROW:
+                            selectRight(evt);
+                            break;
+                    }
+                });
+        }
         
-        return declare("app.WorkItemListWidget", [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, keyboardNavigationMixin], {
+        function initSearch(self) {
+            var searchRest = new Rest(getUrl("/api/WorkItems"), true);
+            var searchTermsObserved = rx.selectProperty(self.model, 'searchTerms');
+
+            function search(searchText) {
+                dojo.addClass(self.txSearch.domNode, "searching");
+                var onCompleted = function () {
+                    dojo.removeClass(self.txSearch.domNode, "searching");
+                };
+
+                return searchRest({ q: searchText, pageSize: 10, pageIndex: 0 })
+                    .asObservable()
+                    .doAction({
+                        onNext: function () { },
+                        onError: onCompleted,
+                        onCompleted: onCompleted
+                    });
+            }
+            
+            function setDefaultSelection() {
+                dojo.query(".selected", self.domNode).removeClass("selected");
+
+                var node = dojo.query(".selectable.work-item:first-of-type", self.domNode);
+                if (node == null)
+                    node = dojo.query(".selectable:first-of-type", self.domNode);
+
+                dojo.addClass(node[0], "selected");
+            }
+            
+            searchTermsObserved
+                .throttle(300)
+                .distinctUntilChanged()
+                .where(function (q) { return q.length > 0; })
+                .select(search)
+                .switchLatest()
+                .subscribe(function (data) {
+                    self.model.set("searchResults", data);
+                    setDefaultSelection();
+                });
+
+            rx.on(self.domNode, ".selectable-action:click").subscribe(function () { alert("aaaa"); });
+
+            searchTermsObserved.subscribe(function (term) {
+                domStyle.set(self.searchResultsPanel, { display: (term.length > 0) ? "block" : "none" });
+            });
+        }
+        
+        return declare("app.WorkItemListWidget", [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
             baseClass: "WorkItemList",
             templateString: template,
+            txSearch: null, // attach-point
+            searchResultsPanel: null, // attach-point
             constructor: function () {
-                this.searchRest = new Rest(getUrl("/api/WorkItems"), true);
                 
                 this.model = mvc.getStateful({
                     searchResults: [],
                     searchTerms: ""
                 });
             },
-            searchRest: null,
-            postCreate: function () {
-                this._searchTermsObserved = rx.selectProperty(this.model, 'searchTerms');
-
-                this._searchTermsObserved
-                    .throttle(300)
-                    .distinctUntilChanged()
-                    .where(function (q) { return q.length > 0; })
-                    .select(dojo.hitch(this,this._search))
-                    .switchLatest()
-                    .subscribe(dojo.hitch(this, function (data) {
-                        this.model.set("searchResults", data);
-                        this._setDefaultSelection();
-                    }));
-
-                rx.on(this.domNode, ".selectable-action:click").subscribe(function () { alert("aaaa"); });
-                
-            },
             startup: function () {
                 this.inherited(arguments);
-                this._searchTermsObserved.subscribe(dojo.hitch(this, function (term) {
-                    domStyle.set(this.searchResultsPanel, {display: (term.length > 0)?"block": "none"});
-                }));
-            },
-            
-            _setDefaultSelection: function () {
-                dojo.query(".selected", this.domNode).removeClass("selected");
-
-                var node = dojo.query(".selectable.work-item:first-of-type", this.domNode);
-                if (node == null)
-                    node = dojo.query(".selectable:first-of-type", this.domNode);
-
-                dojo.addClass(node[0], "selected");
-            },
-            _search: function (searchText) {
-                dojo.addClass(this.txSearch.domNode, "searching");
-                var onCompleted = dojo.hitch(this, function() {
-                    dojo.removeClass(this.txSearch.domNode, "searching");
-                });
-
-                return this.searchRest({ q: searchText, pageSize: 10, pageIndex: 0 })
-                    .asObservable()
-                    .doAction({
-                        onNext: function () { }, 
-                        onError: onCompleted,
-                        onCompleted: onCompleted
-                    });
+                initSearch(this);
+                initKeyboardNavigation(this);
             }
         });
 });
